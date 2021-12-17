a0860a3921e2b88b91102a6a888654e7a17b552d
akchoudhary1980 <akchoudhary1980@rediffmail.com>
2021-12-17 17:14:01 +0530

    Dashboard Add

---
+++ Semec/Areas/TenderQuoteManage/Controllers/DashboardController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Semec.Areas.TenderQuoteManage.Controllers
+{
+    public class DashboardController : Controller
+    {
+        // GET: TenderQuoteManage/Dashboard
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
